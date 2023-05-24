d84ca17a0f2f90ee094da9b8431063abdff69afc
Joshua Vaneps <111782130+JoshuaVaneps@users.noreply.github.com>
2023-05-24 18:25:54 -0400

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,6 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
     var searchQuery = document.getElementById("searchQuery").value;
-    var url = "https://budget.html?searchQuery=" + encodeURIComponent(searchQuery);
+    var url = "budget.html?searchQuery=" + encodeURIComponent(searchQuery);
     window.location.href = url;
   });
